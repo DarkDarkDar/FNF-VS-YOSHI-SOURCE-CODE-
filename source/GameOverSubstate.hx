a79144c70b81c0d5e37d0c6ac6d165d7e0ad0245
DarkDarkDar <95237782+DarkDarkDar@users.noreply.github.com>
2022-08-14 10:30:11 -0300

    Update GameOverSubstate.hx
    Amém

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -64,6 +64,75 @@ class GameOverSubstate extends MusicBeatSubstate
 			if (PlayState.isStoryMode)
 				FlxG.switchState(new StoryMenuState());
 			else
+package;
+
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSubState;
+import flixel.math.FlxPoint;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+
+class GameOverSubstate extends MusicBeatSubstate
+{
+	var bf:Boyfriend;
+	var camFollow:FlxObject;
+
+	var stageSuffix:String = "";
+
+	public function new(x:Float, y:Float)
+	{
+		var daStage = PlayState.curStage;
+		var daBf:String = '';
+		switch (PlayState.SONG.player1)
+		{
+			case 'bf-pixel':
+				stageSuffix = '-pixel';
+				daBf = 'bf-pixel-dead';
+			default:
+				daBf = 'bf';
+		}
+
+		super();
+
+		Conductor.songPosition = 0;
+
+		bf = new Boyfriend(x, y, daBf);
+		add(bf);
+
+		camFollow = new FlxObject(bf.getGraphicMidpoint().x, bf.getGraphicMidpoint().y, 1, 1);
+		add(camFollow);
+
+		FlxG.sound.play(Paths.sound('fnf_loss_sfx' + stageSuffix));
+		Conductor.changeBPM(100);
+
+		// FlxG.camera.followLerp = 1;
+		// FlxG.camera.focusOn(FlxPoint.get(FlxG.width / 2, FlxG.height / 2));
+		FlxG.camera.scroll.set();
+		FlxG.camera.target = null;
+
+		bf.playAnim('firstDeath');
+	
+#if android
+addVirtualPad(NONE, A_B);
+#end
+}
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (controls.ACCEPT)
+		{
+			endBullshit();
+		}
+
+		if (controls.BACK)
+		{
+			FlxG.sound.music.stop();
+
+			if (PlayState.isStoryMode)
+				FlxG.switchState(new StoryMenuState());
+			else
 				FlxG.switchState(new FreeplayState());
 			PlayState.loadRep = false;
 		}
